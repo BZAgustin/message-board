6cfa2b5cc463f55ea5d940d64ce2b7c6624be9fd
BZAgustin <abustoszanelli@gmail.com>
2023-09-11 12:52:50 -0300

    Add message array and router functionality

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+/* eslint-disable prefer-arrow-callback */
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+   {
+     text: "Hi there!",
+     user: "Amando",
+     added: new Date()
+   },
+   {
+     text: "Hello World!",
+     user: "Charles",
+     added: new Date()
+   }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('new', {title: 'New Post'})
+});
+
+router.post('/new', function(req, res, next) {
+  const {username, postContent} = req.body;
+
+  messages.push({text: postContent, user: username, added: new Date()});
+  res.redirect('/');
+});
+
+module.exports = router;
